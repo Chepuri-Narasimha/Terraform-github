080e6e4b9bd0adcf6e2a7eb9c39f7c33f9e3e940
chepuri-narasimha <chepuri.bala208@gmail.com>
2024-02-08 12:47:34 +0530

    added EKS with tf depolyment files

--- VPC/general-variable.tf
+++ VPC/general-variable.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "us-east-1"  
+  default = "eu-west-1"  
 }
 
 variable "Owner" {
