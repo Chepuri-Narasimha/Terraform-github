964b1fe0efaa56f2ac7ec67704fb2576ce4488e7
chepuri-narasimha <chepuri.bala208@gmail.com>
2024-02-08 13:10:51 +0530

    added one more pub subnet

--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -10,7 +10,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   azs                 = data.aws_availability_zones.available.names
   private_subnets     = ["10.0.2.0/24"]
-  public_subnets      = ["10.0.101.0/24"]
+  public_subnets      = ["10.0.101.0/24", "10.0.102,0/24"]
 
  /* # Database Subnets
   database_subnets    = ["10.0.151.0/24"]
