3b3843f8bc5333851e4f5c450da69179907b76e8
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-02-13 18:00:22 +0530

    adding ebs-csi drive

---
+++ VPC/ebs-csi-addon.tf
@@ -0,0 +1,6 @@
+resource "aws_eks_addon" "csi_driver" {
+  cluster_name                = aws_eks_cluster.eks_cluster.name
+  addon_name                  = "aws-ebs-csi-driver"
+# addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
+  service_account_role_arn    = aws_iam_role.eks_ebs_csi_driver.arn
+}
--- VPC/ebs-csi-iam-policy-and-role.tf
+++ VPC/ebs-csi-iam-policy-and-role.tf
@@ -1,4 +1,37 @@
-#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn
+data "aws_iam_policy_document" "csi" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+ /*       Condition = {
+          StringEquals = {            
+            "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+    }*/
+    
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.oidc_provider.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_ebs_csi_driver" {
+  assume_role_policy = data.aws_iam_policy_document.csi.json
+  name               = "${local.name}-ebs-csi-iam-role"
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
+  role       = aws_iam_role.eks_ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
+/*#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn
 #data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn
 
 # Resource: Create EBS CSI IAM Policy 
@@ -52,3 +85,5 @@ output "ebs_csi_iam_role_arn" {
   description = "EBS CSI IAM Role ARN"
   value = aws_iam_role.ebs_csi_iam_role.arn
 }
+
+*/
