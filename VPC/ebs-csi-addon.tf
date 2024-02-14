2ceea4795bb87da16cba91709b84a257299df4f5
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-02-14 11:31:27 +0530

    commented ebs csi

--- VPC/ebs-csi-addon.tf
+++ VPC/ebs-csi-addon.tf
@@ -1,7 +1,8 @@
-resource "aws_eks_addon" "csi_driver" {
+/*resource "aws_eks_addon" "csi_driver" {
   cluster_name                = aws_eks_cluster.eks_cluster.name
   addon_name                  = "aws-ebs-csi-driver"
 # addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
   service_account_role_arn    = aws_iam_role.eks_ebs_csi_driver.arn
-  depends_on = [aws_eks_node_group.eks_ng_public  ]
+#  depends_on = [aws_eks_node_group.eks_ng_public]
 }
+*/
--- VPC/ebs-csi-iam-policy-and-role.tf
+++ VPC/ebs-csi-iam-policy-and-role.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "csi" {
+/*data "aws_iam_policy_document" "csi" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "csi" {
           StringEquals = {            
             "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
     }*/
-    
+/*    
     principals {
       identifiers = [aws_iam_openid_connect_provider.oidc_provider.arn]
       type        = "Federated"
@@ -29,6 +29,7 @@ resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
   role       = aws_iam_role.eks_ebs_csi_driver.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
+*/
 
 
 /*#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn
