4f26986d52f49fc83f4d2f24c7ffaea05b2a7d14
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-02-19 14:31:21 +0530

    adding reclaimpolicy

--- VPC/ebs-csi-addon.tf
+++ VPC/ebs-csi-addon.tf
@@ -3,5 +3,14 @@ resource "aws_eks_addon" "csi_driver" {
   addon_name                  = "aws-ebs-csi-driver"
 # addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
   service_account_role_arn    = aws_iam_role.eks_ebs_csi_driver.arn
-  depends_on = [aws_eks_node_group.eks_ng_public]
+  depends_on = [aws_eks_node_group.eks_ng_public,
+                aws_iam_role_policy_attachment.amazon_ebs_csi_driver]
+}
+
+output "csi_driver_arn" {
+  value = aws_eks_addon.csi_driver.arn 
+}
+
+output "csi_driver_id" {
+  value = aws_eks_addon.csi_driver.id
 }
