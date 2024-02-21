3b9d8c0a60c9185d84cc38906b0b8675a6618636
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-02-21 10:54:52 +0530

    added cluster-role

--- IAM-TF/users.tf
+++ IAM-TF/users.tf
@@ -6,26 +6,18 @@ resource "aws_iam_user" "eksreadonly_user" {
   tags = local.common_tags
 }
 
-# Resource: AWS IAM Group Membership
-resource "aws_iam_group_membership" "eksreadonly" {
-  name = "${local.name}-eksreadonly-group-membership"
-  users = [
-    aws_iam_user.eksreadonly_user.name
-  ]
-  group = aws_iam_group.eksreadonly_iam_group.name
-}
-
 resource "aws_iam_user" "eksreadonly_user1" {
-  name = "${local.name}-eksreadonly1"
+  name = "${local.name}-eksreadonly2"
   path = "/"
   force_destroy = true
   tags = local.common_tags
 }
 
 # Resource: AWS IAM Group Membership
-resource "aws_iam_group_membership" "eksreadonly1" {
-  name = "${local.name}-eksreadonly-group-membership1"
+resource "aws_iam_group_membership" "eksreadonly" {
+  name = "${local.name}-eksreadonly-group-membership"
   users = [
+    aws_iam_user.eksreadonly_user.name,
     aws_iam_user.eksreadonly_user1.name
   ]
   group = aws_iam_group.eksreadonly_iam_group.name
