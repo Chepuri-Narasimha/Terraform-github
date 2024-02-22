d341baf02532b3b789107995546005be912a0412
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-02-22 12:52:36 +0530

    added albcontorllers

--- EKS-Cluster/LBController-addon/lbc-helm-kube-provider.tf
+++ EKS-Cluster/LBController-addon/lbc-helm-kube-provider.tf
@@ -1,3 +1,8 @@
+# Datasource: EKS Cluster Auth 
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
 #Kubernetes Provider
 provider "kubernetes" {
   host = data.terraform_remote_state.eks.outputs.cluster_endpoint 
@@ -5,16 +10,12 @@ provider "kubernetes" {
   token = data.aws_eks_cluster_auth.cluster.token
 }
 
-# Datasource: EKS Cluster Auth 
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
-}
 
 # HELM Provider
 provider "helm" {
   kubernetes {
     host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    token                  = data.terraform_remote_state.eks.outputs.token
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
