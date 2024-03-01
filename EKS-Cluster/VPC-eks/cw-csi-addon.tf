70c01ed83ece8f4fb0966f0dac731c40755bd046
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-03-01 15:44:44 +0530

    adding cloudwatch agent policy

---
+++ EKS-Cluster/VPC-eks/cw-csi-addon.tf
@@ -0,0 +1,16 @@
+resource "aws_eks_addon" "cw_csi_addon" {
+  addon_name   = "amazon-cloudwatch-observability"
+  cluster_name = aws_eks_cluster.eks_cluster.name
+# addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
+  service_account_role_arn    = aws_iam_role.eks_cw_csi_driver.arn
+  depends_on = [aws_eks_node_group.eks_ng_private,
+                aws_iam_role_policy_attachment.amazon_cw_csi_driver]
+}
+
+output "cw_csi_addon_arn" {
+  value = aws_eks_addon.cw_csi_addon.arn 
+}
+
+output "csi_driver_id" {
+  value = aws_eks_addon.cw_csi_addon.id
+}
---
+++ EKS-Cluster/VPC-eks/cw-csi-iampolicy-role.tf
@@ -0,0 +1,90 @@
+data "aws_iam_policy_document" "cw_csi" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub"
+      values   = ["system:serviceaccount:kube-system:cw-csi-controller-sa"]
+    }
+ /*       Condition = {
+          StringEquals = {            
+            "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+    }*/
+  
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.oidc_provider.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_cw_csi_driver" {
+  assume_role_policy = data.aws_iam_policy_document.cw_csi.json
+  name               = "${local.name}-cw-csi-iam-role"
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_cw_csi_driver" {
+  role       = aws_iam_role.eks_cw_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+
+
+/*#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn
+#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn
+
+# Resource: Create EBS CSI IAM Policy 
+resource "aws_iam_policy" "ebs_csi_iam_policy" {
+  name        = "${local.name}-AmazonEKS_EBS_CSI_Driver_Policy"
+  path        = "/"
+  description = "EBS CSI IAM Policy"
+  policy = data.http.ebs_csi_iam_policy.body
+}
+
+output "ebs_csi_iam_policy_arn" {
+  value = aws_iam_policy.ebs_csi_iam_policy.arn 
+}
+
+# Resource: Create IAM Role and associate the EBS IAM Policy to it
+resource "aws_iam_role" "ebs_csi_iam_role" {
+  name = "${local.name}-ebs-csi-iam-role"
+
+  # Terraform's "jsonencode" function converts a Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${aws_iam_openid_connect_provider.oidc_provider.arn}"
+        }
+        Condition = {
+          StringEquals = {            
+            "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }        
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${local.name}-ebs-csi-iam-role"
+  }
+}
+
+# Associate EBS CSI IAM Policy to EBS CSI IAM Role
+resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
+  policy_arn = aws_iam_policy.ebs_csi_iam_policy.arn 
+  role       = aws_iam_role.ebs_csi_iam_role.name
+}
+
+output "ebs_csi_iam_role_arn" {
+  description = "EBS CSI IAM Role ARN"
+  value = aws_iam_role.ebs_csi_iam_role.arn
+}
+
+*/
