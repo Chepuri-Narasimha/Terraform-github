32e5bc44277e493e07cdacdfb1aa9b43fe37ea0e
Chepuri Bala Narasimha <bala@Jayasimhas-MacBook-Pro.local>
2024-04-04 15:10:41 +0530

    adding db subnet

--- EKS-Cluster/VPC-eks/vpc.tf
+++ EKS-Cluster/VPC-eks/vpc.tf
@@ -17,8 +17,8 @@ module "vpc" {
   create_database_subnet_group = true
   create_database_subnet_route_table= true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+ # enable_nat_gateway = true
+  #single_nat_gateway = true
   enable_dns_hostnames = true
   enable_dns_support = true
 
