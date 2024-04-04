a54bc75089cbca5ec3f1285dd45eb8b0a11d9ba1
Chepuri Bala Narasimha <bala@Jayasimhas-MacBook-Pro.local>
2024-04-04 14:58:57 +0530

    adding db subnet

--- EKS-Cluster/VPC-eks/vpc.tf
+++ EKS-Cluster/VPC-eks/vpc.tf
@@ -9,7 +9,7 @@ module "vpc" {
   name = "VPC-DEV"
   cidr = "10.0.0.0/16"
   azs                 = data.aws_availability_zones.available.names
-  private_subnets     = ["10.0.2.0/24"]
+  #private_subnets     = ["10.0.2.0/24"]
   public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
 
  #Database Subnets
@@ -28,11 +28,11 @@ module "vpc" {
     "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
   }
 
-  private_subnet_tags = {
+  /*private_subnet_tags = {
     Type = "private-subnets"
     "kubernetes.io/role/internal-elb" = 1    
     "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"  
-  }
+  }*/
   database_subnet_tags = {
     Type = "database-subnets"
   }
