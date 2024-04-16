fcdc917d7917403c7dcf00a3cdb7ab9dd4e7468a
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2024-04-16 17:52:15 +0530

    added metadata to cw-csi-addon

--- EKS-Cluster/VPC-eks/cw-csi-addon.tf
+++ EKS-Cluster/VPC-eks/cw-csi-addon.tf
@@ -3,7 +3,7 @@ resource "aws_eks_addon" "cw_csi_addon" {
   cluster_name = aws_eks_cluster.eks_cluster.name
 # addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
   service_account_role_arn    = aws_iam_role.eks_cw_csi_driver.arn
-  depends_on = [aws_eks_node_group.eks_ng_public,
+  depends_on = [aws_eks_node_group.eks_ng_private,
                 aws_iam_role_policy_attachment.amazon_cw_csi_driver]
 }
 
